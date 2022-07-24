66fdaacdaddf1820b621520db4c19fa9ae4383a0
ReanGD <reangd@yandex.ru>
2022-07-25 00:55:37 +0300

    add filter for command storage

--- ui/lib/model/cmd_loader.dart
+++ ui/lib/model/cmd_loader.dart
@@ -1,10 +1,20 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:runify/model/command.dart';
+import 'package:runify/model/cmd.dart';
 
-class CommandStorage {
-  Future<List<Command>> get() async {
+class CommandLoader {
+  late Future<List<Command>> _commands;
+
+  CommandLoader._() {
+    _commands = _get();
+  }
+
+  static final CommandLoader instance = CommandLoader._();
+
+  Future<List<Command>> get commands => _commands;
+
+  Future<List<Command>> _get() async {
     final extensionPath =
         "${Directory.current.parent.path}/extensions/applications.py";
     final result = await Process.run("python", [extensionPath]);
