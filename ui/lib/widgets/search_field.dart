09d270595b7f20e6c1ab9da3ec5c8a765aae27d1
ReanGD <reangd@yandex.ru>
2022-08-02 05:24:43 +0300

    add search field

---
+++ ui/lib/widgets/search_field.dart
@@ -0,0 +1,51 @@
+import 'package:runify/style.dart';
+import 'package:flutter/material.dart';
+
+class SearchField extends StatelessWidget {
+  final String hintText;
+  final EdgeInsetsGeometry? padding;
+  final ValueChanged<String> onChanged;
+
+  const SearchField({
+    super.key,
+    required this.hintText,
+    this.padding,
+    required this.onChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    const border = UnderlineInputBorder(
+      borderSide: BorderSide.none,
+    );
+
+    final child = TextField(
+      autofocus: true,
+      style: theme.textTheme.majorText,
+      textAlignVertical: TextAlignVertical.center,
+      decoration: InputDecoration(
+        hintText: hintText,
+        hintStyle: theme.textTheme.minorText,
+        filled: false,
+        errorBorder: border,
+        focusedBorder: border,
+        focusedErrorBorder: border,
+        disabledBorder: border,
+        enabledBorder: border,
+        border: border,
+        contentPadding: const EdgeInsets.all(0),
+      ),
+      onChanged: onChanged,
+    );
+
+    if (padding != null) {
+      return Padding(
+        padding: padding!,
+        child: child,
+      );
+    }
+
+    return child;
+  }
+}
