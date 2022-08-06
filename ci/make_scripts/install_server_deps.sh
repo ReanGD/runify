6d8e12b6f34b611f57f17bbf081f9fb2818f0d33
ReanGD <reangd@yandex.ru>
2022-08-06 04:51:50 +0300

    add version of liner

--- ci/make_scripts/install_server_deps.sh
+++ ci/make_scripts/install_server_deps.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+GOLANGCILINT_VERSION=${1}
+
 go mod download
 
 GOPATH_BIN=$(go env GOPATH | cut -d ':' -f1)/bin
@@ -8,5 +10,4 @@ go install github.com/sqs/goreturns@latest
 go install github.com/vektra/mockery/v2/...@v2.11.0
 go install github.com/boyter/scc@a500fe2f535d421c03006c6521be5e8d8984291f
 
-GOLANGCILINT_VERSION="v1.48.0"
 (cd ..; wget -O - -q  https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b ${GOPATH_BIN} ${GOLANGCILINT_VERSION})
