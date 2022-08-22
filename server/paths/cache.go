8e47affb6b73464ed886cd0e2d9fdca9a9dfa933
ReanGD <reangd@yandex.ru>
2022-08-22 17:42:31 +0300

    add cache for svg icons

--- server/paths/cache.go
+++ server/paths/cache.go
@@ -5,14 +5,56 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 
 	"github.com/ReanGD/runify/server/gtk"
+	"github.com/ReanGD/runify/server/logger"
 )
 
 const (
 	appName = "runify"
 )
+
+type iconKey struct {
+	size int
+	name string
+}
+
+func newIconKey(size int, name string) iconKey {
+	return iconKey{
+		size: size,
+		name: name,
+	}
+}
+
+func newIconKeyFromPath(fullpath string) (iconKey, error) {
+	_, filename := filepath.Split(fullpath)
+	extIndex := strings.LastIndexByte(filename, '.')
+	if extIndex == -1 {
+		return iconKey{}, fmt.Errorf("Wrong icon cache filename %s, error: not found extension", fullpath)
+	}
+	filename = filename[:extIndex]
+
+	sepIndex := strings.IndexByte(filename, '_')
+	if sepIndex == -1 {
+		return iconKey{}, fmt.Errorf("Wrong icon cache filename %s, error: not found separator", fullpath)
+	}
+
+	size, err := strconv.Atoi(filename[:sepIndex])
+	if err != nil {
+		return iconKey{}, fmt.Errorf("Wrong icon cache filename %s, error: can't parse size: %s", fullpath, err)
+	}
+
+	return iconKey{
+		size: size,
+		name: filename[sepIndex+1:],
+	}, nil
+}
+
+func (k iconKey) toFullPath() string {
+	return filepath.Join(GetAppIconCache(), fmt.Sprintf("%d_%s.png", k.size, k.name))
+}
 
 type cachePaths struct {
 	defaultIconTheme *gtk.IconTheme
@@ -21,6 +63,7 @@ type cachePaths struct {
 	userCache        string
 	appCache         string
 	appIconCache     string
+	iconPathCache    map[iconKey]string
 	xdgDataDirs      []string
 	xdgAppDirs       []string
 }
@@ -67,6 +110,18 @@ func getXDGAppDirs(xdgDataDirs []string) []string {
 	return xdgAppDirs
 }
 
+func scanIcons(dirPath string) {
+	Walk(dirPath, func(fullpath string, mode PathMode) {
+		if mode == ModeRegFile {
+			if key, err := newIconKeyFromPath(fullpath); err != nil {
+				logger.Write(err.Error())
+			} else {
+				cache.iconPathCache[key] = fullpath
+			}
+		}
+	})
+}
+
 func Init() error {
 	var ok bool
 	var err error
@@ -101,12 +156,15 @@ func Init() error {
 	}
 
 	cache.appIconCache = filepath.Join(cache.appCache, "icon")
+	cache.iconPathCache = make(map[iconKey]string)
 	if ok, err = ExistsDir(cache.appIconCache); err != nil {
 		return fmt.Errorf("Getting info about appIconCache dir (%s) ended with error: %s", cache.appIconCache, err)
 	} else if !ok {
 		if err = os.MkdirAll(cache.appIconCache, 0700); err != nil {
 			return fmt.Errorf("Creating appIconCache dir (%s) ended with error: %s", cache.appIconCache, err)
 		}
+	} else {
+		defer scanIcons(cache.appIconCache)
 	}
 
 	cache.xdgDataDirs = getXDGDataDirs()
