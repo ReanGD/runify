fedda5876dba84114f0843253e72b48274b46fe6
ReanGD <reangd@yandex.ru>
2022-08-24 02:50:43 +0300

    fix generator protobuf

--- ci/make_scripts/proto.sh
+++ ci/make_scripts/proto.sh
@@ -8,5 +8,5 @@ PROTO_DIR=$PROJECT_DIR/proto
 IMAGE_VERSION=${1}
 
 cd "${PROJECT_DIR}"
-docker run --rm -it -v `pwd`:/project:rw -w /project -u $(id -u ${USER}):$(id -g ${USER}) --network none runify_protoc_gen_go:${IMAGE_VERSION} protoc --go_out=./server ./proto/runify.proto
-docker run --rm -it -v `pwd`:/project:rw -w /project -u $(id -u ${USER}):$(id -g ${USER}) --network none runify_protoc_gen_dart:${IMAGE_VERSION} protoc --dart_out=./ui/lib ./proto/runify.proto
+docker run --rm -it -v `pwd`:/project:rw -w /project -u $(id -u ${USER}):$(id -g ${USER}) --network none runify_protoc_gen_go:${IMAGE_VERSION} protoc --go_out=./server --go-grpc_out=./server ./proto/runify.proto
+docker run --rm -it -v `pwd`:/project:rw -w /project -u $(id -u ${USER}):$(id -g ${USER}) --network none runify_protoc_gen_dart:${IMAGE_VERSION} protoc --dart_out=grpc:./ui/lib/pb --proto_path=proto ./proto/runify.proto
