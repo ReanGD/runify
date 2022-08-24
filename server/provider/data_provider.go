6fb6a761a1f06eeb36a401465d90fe1a4725634b
ReanGD <reangd@yandex.ru>
2022-08-24 23:39:13 +0300

    add wrapper for data providers

---
+++ server/provider/data_provider.go
@@ -0,0 +1,140 @@
+package provider
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"reflect"
+	"sync"
+
+	"github.com/ReanGD/runify/server/config"
+	"github.com/ReanGD/runify/server/logger"
+	"github.com/ReanGD/runify/server/pb"
+	"github.com/ReanGD/runify/server/system/module"
+	"go.uber.org/zap"
+)
+
+type dataProviderHandler interface {
+	getName() string
+	init(cfg *config.Config, moduleLogger *zap.Logger, providerID uint64) error
+	start()
+	getRoot() []*pb.Command
+}
+
+type dataProvider struct {
+	providerID uint64
+	handler    dataProviderHandler
+
+	module.Module
+}
+
+func newDataProvider(providerID uint64, handler dataProviderHandler) *dataProvider {
+	return &dataProvider{
+		providerID: providerID,
+		handler:    handler,
+	}
+}
+
+func (p *dataProvider) init(cfg *config.Config, rootProviderLogger *zap.Logger) <-chan error {
+	ch := make(chan error)
+	go func() {
+		channelLen := cfg.GetConfiguration().Provider.SubModuleChannelLen
+		p.InitSubmodule(rootProviderLogger, p.handler.getName(), channelLen)
+
+		ch <- p.handler.init(cfg, p.ModuleLogger, p.providerID)
+	}()
+
+	return ch
+}
+
+func (p *dataProvider) start(ctx context.Context, wg *sync.WaitGroup) <-chan error {
+	wg.Add(1)
+	ch := make(chan error)
+	go func() {
+		p.handler.start()
+
+		for {
+			if isFinish, err := p.safeRequestLoop(ctx); isFinish {
+				ch <- err
+				wg.Done()
+				return
+			}
+		}
+	}()
+
+	return ch
+}
+
+func (p *dataProvider) safeRequestLoop(ctx context.Context) (resultIsFinish bool, resultErr error) {
+	var request interface{}
+	defer func() {
+		if recoverResult := recover(); recoverResult != nil {
+			if request != nil {
+				reason := p.RecoverLog(recoverResult, request)
+				resultIsFinish, resultErr = p.onRequestDefault(request, reason)
+			} else {
+				_ = p.RecoverLog(recoverResult, "unknown request")
+			}
+		}
+	}()
+
+	messageCh := p.GetReadChannel()
+	done := ctx.Done()
+	for {
+		request = nil
+		select {
+		case <-done:
+			resultIsFinish = true
+			resultErr = nil
+			return
+		case request = <-messageCh:
+			p.MessageWasRead()
+			if resultIsFinish, resultErr = p.onRequest(request); resultIsFinish {
+				return
+			}
+		}
+	}
+}
+
+func (p *dataProvider) onRequest(request interface{}) (bool, error) {
+	switch r := request.(type) {
+	case *getRootCmd:
+		r.result <- p.handler.getRoot()
+
+	default:
+		p.ModuleLogger.Warn("Unknown message received",
+			zap.String("Request", fmt.Sprintf("%v", request)),
+			zap.Stringer("RequestType", reflect.TypeOf(request)),
+			logger.LogicalError)
+		return true, errors.New("unknown message received")
+	}
+
+	return false, nil
+}
+
+func (p *dataProvider) onRequestDefault(request interface{}, reason string) (bool, error) {
+	switch r := request.(type) {
+	case *getRootCmd:
+		r.result <- []*pb.Command{}
+		p.ModuleLogger.Debug("Message is wrong",
+			zap.String("RequestType", "getRoot"),
+			zap.String("Reason", reason),
+			zap.String("Action", "skip request"))
+
+	default:
+		p.ModuleLogger.Warn("Unknown message received",
+			zap.String("Request", fmt.Sprintf("%v", request)),
+			zap.String("Reason", reason),
+			zap.Stringer("RequestType", reflect.TypeOf(request)),
+			logger.LogicalError)
+		return true, errors.New("unknown message received")
+	}
+
+	return false, nil
+}
+
+func (p *dataProvider) getRoot(result chan<- []*pb.Command) {
+	p.AddToChannel(&getRootCmd{
+		result: result,
+	})
+}
