0f69f43e759baffbccb93dec2359c2296b17ac13
ReanGD <reangd@yandex.ru>
2022-08-25 03:14:41 +0300

    add parent class for module

---
+++ server/system/module/module.go
@@ -0,0 +1,102 @@
+package module
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"sync/atomic"
+
+	"github.com/ReanGD/runify/server/logger"
+	"go.uber.org/zap"
+)
+
+type Channel struct {
+	messageCh chan interface{}
+
+	queueOverflow int32
+}
+
+func (c *Channel) Init(channelLen uint32) {
+	c.messageCh = make(chan interface{}, channelLen)
+	c.queueOverflow = 0
+}
+
+func (c *Channel) GetReadChannel() <-chan interface{} {
+	return c.messageCh
+}
+
+func (c *Channel) AddToChannel(value interface{}) {
+	select {
+	case c.messageCh <- value:
+		atomic.StoreInt32(&c.queueOverflow, 0)
+	default:
+		atomic.StoreInt32(&c.queueOverflow, 1)
+		c.messageCh <- value
+	}
+}
+
+func (c *Channel) MessageWasRead() {
+
+}
+
+func (c *Channel) ClearModuleQueue() {
+	for {
+		select {
+		case <-c.messageCh:
+			c.MessageWasRead()
+		default:
+			return
+		}
+	}
+}
+
+func (c *Channel) IsOverflow() bool {
+	return atomic.LoadInt32(&c.queueOverflow) == 1
+}
+
+type Module struct {
+	RootLogger   *zap.Logger
+	ModuleLogger *zap.Logger
+
+	Channel
+}
+
+func (m *Module) Init(rootLogger *zap.Logger, moduleName string, channelLen uint32) {
+	m.RootLogger = rootLogger
+	m.ModuleLogger = rootLogger.With(zap.String("module", moduleName))
+	m.Channel.Init(channelLen)
+}
+
+func (m *Module) InitSubmodule(rootLogger *zap.Logger, submoduleName string, channelLen uint32) {
+	m.RootLogger = rootLogger
+	m.ModuleLogger = rootLogger.With(zap.String("module", submoduleName))
+	m.Channel.Init(channelLen)
+}
+
+func (m *Module) RecoverLog(recoverResult interface{}, request interface{}) string {
+	var err error
+	switch val := recoverResult.(type) {
+	case string:
+		err = errors.New(val)
+	case error:
+		err = val
+	default:
+		err = fmt.Errorf("%v", val)
+	}
+
+	if request != nil {
+		m.ModuleLogger.Error("Panic during message processing",
+			zap.String("Request", fmt.Sprintf("%v", request)),
+			zap.Stringer("RequestType", reflect.TypeOf(request)),
+			zap.Error(err),
+			logger.LogicalError)
+	} else {
+		m.ModuleLogger.Error("Panic during message processing",
+			zap.String("Request", "unknown"),
+			zap.String("RequestType", "unknown"),
+			zap.Error(err),
+			logger.LogicalError)
+	}
+
+	return "panic during message processing: " + err.Error()
+}
