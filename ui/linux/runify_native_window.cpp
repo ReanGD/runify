3ca64a519e61e49679739951e7c3ed777e295f34
ReanGD <reangd@yandex.ru>
2022-09-24 03:52:40 +0300

    add signals to plugin

--- ui/linux/runify_native_window.cpp
+++ ui/linux/runify_native_window.cpp
@@ -2,14 +2,14 @@
 
 
 struct Geometry {
-    float x;
-    float y;
-    float width;
-    float height;
+  float x;
+  float y;
+  float width;
+  float height;
 
-    Geometry() = default;
-    Geometry(FlValue* args);
-    FlMethodResponse* ToFlResponse();
+  Geometry() = default;
+  Geometry(FlValue* args);
+  FlMethodResponse* ToFlResponse();
 };
 
 Geometry::Geometry(FlValue* args)
@@ -21,54 +21,109 @@ Geometry::Geometry(FlValue* args)
 }
 
 FlMethodResponse* Geometry::ToFlResponse() {
-    g_autoptr(FlValue) result = fl_value_new_map();
-    fl_value_set_string_take(result, "x", fl_value_new_float(x));
-    fl_value_set_string_take(result, "y", fl_value_new_float(y));
-    fl_value_set_string_take(result, "width", fl_value_new_float(width));
-    fl_value_set_string_take(result, "height", fl_value_new_float(height));
+  g_autoptr(FlValue) result = fl_value_new_map();
+  fl_value_set_string_take(result, "x", fl_value_new_float(x));
+  fl_value_set_string_take(result, "y", fl_value_new_float(y));
+  fl_value_set_string_take(result, "width", fl_value_new_float(width));
+  fl_value_set_string_take(result, "height", fl_value_new_float(height));
 
-    return FL_METHOD_RESPONSE(fl_method_success_response_new(result));
+  return FL_METHOD_RESPONSE(fl_method_success_response_new(result));
 }
 
 static FlMethodResponse* flBool(bool value) {
-    g_autoptr(FlValue) result = fl_value_new_bool(value);
-    return FL_METHOD_RESPONSE(fl_method_success_response_new(result));
+  g_autoptr(FlValue) result = fl_value_new_bool(value);
+  return FL_METHOD_RESPONSE(fl_method_success_response_new(result));
 }
 
 static FlMethodResponse* flNotImplemented() {
-    return FL_METHOD_RESPONSE(fl_method_not_implemented_response_new());
+  return FL_METHOD_RESPONSE(fl_method_not_implemented_response_new());
 }
 
-RNWindow::RNWindow(GtkWindow* gtk_window)
-    : m_gtk_window(gtk_window)
-    , m_gdk_window(gtk_widget_get_window(GTK_WIDGET(gtk_window))) {
+RNWindow* RNWindow::instance = nullptr;
 
-    GdkDisplay* display = gdk_display_get_default();
-    GdkMonitor* monitor = gdk_display_get_monitor_at_window(display, m_gdk_window);
-    GdkRectangle frame;
-    gdk_monitor_get_geometry(monitor, &frame);
+RNWindow::RNWindow(GtkWindow* gtk_window, FlMethodChannel* channel)
+  : m_gtk_window(gtk_window)
+  , m_gdk_window(gtk_widget_get_window(GTK_WIDGET(gtk_window)))
+  , m_channel(channel) {
 
-    m_monitor_hwidth_px = frame.width / 2;
-    m_monitor_hheight_px = frame.height / 2;
-    m_window_width_ppm = static_cast<float>(frame.width) / static_cast<float>(gdk_monitor_get_width_mm(monitor));
-    m_window_height_ppm = static_cast<float>(frame.height) / static_cast<float>(gdk_monitor_get_height_mm(monitor));
+  RNWindow::instance = this;
+
+  GdkDisplay* display = gdk_display_get_default();
+  GdkMonitor* monitor = gdk_display_get_monitor_at_window(display, m_gdk_window);
+  GdkRectangle frame;
+  gdk_monitor_get_geometry(monitor, &frame);
+
+  m_monitor_hwidth_px = frame.width / 2;
+  m_monitor_hheight_px = frame.height / 2;
+  m_window_width_ppm = static_cast<float>(frame.width) / static_cast<float>(gdk_monitor_get_width_mm(monitor));
+  m_window_height_ppm = static_cast<float>(frame.height) / static_cast<float>(gdk_monitor_get_height_mm(monitor));
 }
 
-void RNWindow::Init(const Geometry& g) {
+RNWindow::~RNWindow() {
+  RNWindow::instance = nullptr;
+}
+
+gboolean on_window_close(GtkWidget* widget, GdkEvent* event, gpointer data) {
+  if ((RNWindow::instance != nullptr) && (!RNWindow::instance->EnableClose())) {
+    RNWindow::instance->EmitEvent("try_close");
+    return FALSE;
+  }
+
+  return TRUE;
+}
+
+gboolean on_window_focus(GtkWidget* widget, GdkEvent* event, gpointer data) {
+  if (RNWindow::instance != nullptr) {
+    RNWindow::instance->EmitEvent("focus");
+  }
+  return false;
+}
+
+gboolean on_window_unfocus(GtkWidget* widget, GdkEvent* event, gpointer data) {
+  if (RNWindow::instance != nullptr) {
+    RNWindow::instance->EmitEvent("unfocus");
+  }
+
+  return false;
+}
+
+static gboolean on_window_resize(GtkWidget* widget, GdkEvent* event, gpointer data) {
+  if (RNWindow::instance != nullptr) {
+    RNWindow::instance->EmitEvent("resize");
+  }
+
+  return false;
+}
+
+static gboolean on_window_move(GtkWidget* widget, GdkEvent* event, gpointer data) {
+  if (RNWindow::instance != nullptr) {
+    RNWindow::instance->EmitEvent("move");
+  }
+
+  return false;
+}
+
+void RNWindow::InitWindow(const Geometry& g) const {
   SetGeometry(g);
   SetGeometryHint(480, 640);
+
+  g_signal_connect(m_gtk_window, "delete_event", G_CALLBACK(on_window_close), nullptr);
+  g_signal_connect(m_gtk_window, "focus-in-event", G_CALLBACK(on_window_focus), nullptr);
+  g_signal_connect(m_gtk_window, "focus-out-event", G_CALLBACK(on_window_unfocus), nullptr);
+  g_signal_connect(m_gtk_window, "check-resize", G_CALLBACK(on_window_resize), nullptr);
+  g_signal_connect(m_gtk_window, "configure-event", G_CALLBACK(on_window_move), nullptr);
 }
 
-bool RNWindow::IsVisible() {
+bool RNWindow::IsVisible() const {
   return gtk_widget_is_visible(GTK_WIDGET(m_gtk_window));
 }
 
-void RNWindow::Show() {
+void RNWindow::Show() const {
   gtk_widget_show(GTK_WIDGET(m_gtk_window));
   gtk_window_present(m_gtk_window);
 }
 
-void RNWindow::Hide() {
+void RNWindow::Hide() const {
   gtk_widget_hide(GTK_WIDGET(m_gtk_window));
 }
 
@@ -77,47 +132,53 @@ void RNWindow::Close() {
   gtk_window_close(m_gtk_window);
 }
 
-bool RNWindow::IsFocused() {
+bool RNWindow::IsFocused() const {
   return gtk_window_is_active(m_gtk_window);
 }
 
-void RNWindow::Focus() {
+void RNWindow::Focus() const {
   gtk_window_present(m_gtk_window);
 }
 
-void RNWindow::SetOpacity(double opacity) {
-    gtk_widget_set_opacity(GTK_WIDGET(m_gtk_window), opacity);
+void RNWindow::SetOpacity(double opacity) const {
+  gtk_widget_set_opacity(GTK_WIDGET(m_gtk_window), opacity);
 }
 
-void RNWindow::GetGeometry(Geometry& g) {
-    gint x_px, y_px, width_px, height_px;
-    gtk_window_get_position(m_gtk_window, &x_px, &y_px);
-    gtk_window_get_size(m_gtk_window, &width_px, &height_px);
+void RNWindow::GetGeometry(Geometry& g) const {
+  gint x_px, y_px, width_px, height_px;
+  gtk_window_get_position(m_gtk_window, &x_px, &y_px);
+  gtk_window_get_size(m_gtk_window, &width_px, &height_px);
 
-    // Result is in millimeter
-    g.x = static_cast<float>(x_px + width_px / 2 - m_monitor_hwidth_px) / m_window_width_ppm;
-    g.y = static_cast<float>(y_px + height_px / 2 - m_monitor_hheight_px) / m_window_height_ppm;
-    g.width = static_cast<float>(width_px) / m_window_width_ppm;
-    g.height = static_cast<float>(height_px) / m_window_height_ppm;
+  // Result is in millimeter
+  g.x = static_cast<float>(x_px + width_px / 2 - m_monitor_hwidth_px) / m_window_width_ppm;
+  g.y = static_cast<float>(y_px + height_px / 2 - m_monitor_hheight_px) / m_window_height_ppm;
+  g.width = static_cast<float>(width_px) / m_window_width_ppm;
+  g.height = static_cast<float>(height_px) / m_window_height_ppm;
 }
 
-void RNWindow::SetGeometry(const Geometry& g) {
-    gint width_px = static_cast<gint>(g.width * m_window_width_ppm + 0.5);
-    gint height_px = static_cast<gint>(g.height * m_window_height_ppm + 0.5);
-    gint x_px = static_cast<gint>((g.x - g.width * 0.5) * m_window_width_ppm + 0.5) + m_monitor_hwidth_px;
-    gint y_px = static_cast<gint>((g.y - g.height * 0.5) * m_window_height_ppm + 0.5) + m_monitor_hheight_px;
+void RNWindow::SetGeometry(const Geometry& g) const {
+  gint width_px = static_cast<gint>(g.width * m_window_width_ppm + 0.5);
+  gint height_px = static_cast<gint>(g.height * m_window_height_ppm + 0.5);
+  gint x_px = static_cast<gint>((g.x - g.width * 0.5) * m_window_width_ppm + 0.5) + m_monitor_hwidth_px;
+  gint y_px = static_cast<gint>((g.y - g.height * 0.5) * m_window_height_ppm + 0.5) + m_monitor_hheight_px;
 
-    gdk_window_move_resize(m_gdk_window, x_px, y_px, width_px, height_px);
+  gdk_window_move_resize(m_gdk_window, x_px, y_px, width_px, height_px);
 }
 
-void RNWindow::SetGeometryHint(int min_width, int min_height) {
-    GdkGeometry geometry;
-    geometry.min_width = min_width;
-    geometry.min_height = min_height;
-    geometry.max_width = G_MAXINT;
-    geometry.max_height = G_MAXINT;
+void RNWindow::SetGeometryHint(int min_width, int min_height) const {
+  GdkGeometry geometry;
+  geometry.min_width = min_width;
+  geometry.min_height = min_height;
+  geometry.max_width = G_MAXINT;
+  geometry.max_height = G_MAXINT;
 
-    gdk_window_set_geometry_hints(m_gdk_window, &geometry, static_cast<GdkWindowHints>(GDK_HINT_MIN_SIZE | GDK_HINT_MAX_SIZE));
+  gdk_window_set_geometry_hints(m_gdk_window, &geometry, static_cast<GdkWindowHints>(GDK_HINT_MIN_SIZE | GDK_HINT_MAX_SIZE));
+}
+
+void RNWindow::EmitEvent(const char* event_name) const {
+  g_autoptr(FlValue) result_data = fl_value_new_map();
+  fl_value_set_string_take(result_data, "eventName", fl_value_new_string(event_name));
+  fl_method_channel_invoke_method(m_channel, "onEvent", result_data, nullptr, nullptr, nullptr);
 }
 
 void RNWindow::HandleMethodCall(FlMethodCall* method_call) {
@@ -126,9 +187,9 @@ void RNWindow::HandleMethodCall(FlMethodCall* method_call) {
   const gchar* method = fl_method_call_get_name(method_call);
   FlValue* args = fl_method_call_get_args(method_call);
 
-  if (strcmp(method, "init") == 0) {
+  if (strcmp(method, "initWindow") == 0) {
     Geometry geometry(args);
-    Init(geometry);
+    InitWindow(geometry);
     response = flBool(true);
   } else if (strcmp(method, "isVisible") == 0) {
     response = flBool(IsVisible());
--- ui/linux/runify_native_window.h
+++ ui/linux/runify_native_window.h
@@ -6,31 +6,34 @@
 struct Geometry;
 class RNWindow {
 public:
-    RNWindow(GtkWindow* gtk_window);
-    ~RNWindow() = default;
+    RNWindow(GtkWindow* gtk_window, FlMethodChannel* channel);
+    ~RNWindow();
 
 public:
-    void Init(const Geometry& g);
-    bool IsVisible();
-    void Show();
-    void Hide();
+    void InitWindow(const Geometry& g) const;
+    bool IsVisible() const;
+    void Show() const;
+    void Hide() const;
     void Close();
-    bool IsFocused();
-    void Focus();
-    void SetOpacity(double opacity);
-    void GetGeometry(Geometry& g);
-    void SetGeometry(const Geometry& g);
-    void SetGeometryHint(int min_width, int min_height);
+    bool IsFocused() const;
+    void Focus() const;
+    void SetOpacity(double opacity) const;
+    void GetGeometry(Geometry& g) const;
+    void SetGeometry(const Geometry& g) const;
+    void SetGeometryHint(int min_width, int min_height) const;
+
+public:
+    bool EnableClose() const { return m_enable_close; }
+    void EmitEvent(const char* event_name) const;
     void HandleMethodCall(FlMethodCall* method_call);
 
 public:
-    bool EnableClose() const {
-        return m_enable_close;
-    }
+    static RNWindow* instance;
 
 private:
     GtkWindow* m_gtk_window = nullptr;
     GdkWindow* m_gdk_window = nullptr;
+    FlMethodChannel* m_channel = nullptr;
 
     int m_monitor_hwidth_px;
     int m_monitor_hheight_px;
