b249eb7f63b1792b66cfae973d98bc53617757b3
ReanGD <reangd@yandex.ru>
2022-11-24 03:10:19 +0300

    new proto messages

--- ci/make_scripts/proto.sh
+++ ci/make_scripts/proto.sh
@@ -9,5 +9,5 @@ IMAGE_VERSION=${1}
 
 cd "${PROJECT_DIR}"
 docker run --rm -it -v `pwd`:/project:rw -w /project -u $(id -u ${USER}):$(id -g ${USER}) --network none runify_protoc_gen_go:${IMAGE_VERSION} protoc --go_out=./server --go-grpc_out=./server ./proto/runify.proto
-mkdir ./ui/lib/pb
+mkdir -p ./ui/lib/pb
 docker run --rm -it -v `pwd`:/project:rw -w /project -u $(id -u ${USER}):$(id -g ${USER}) --network none runify_protoc_gen_dart:${IMAGE_VERSION} protoc --dart_out=grpc:./ui/lib/pb --proto_path=proto ./proto/runify.proto
